d8c0a3881cb27a8c26d6c289db48d8f21b875b20
dyson1602 <cclark000@citymail.cuny.edu>
2020-11-15 12:14:53 -0500

    all tests work. live server works also, you just need to give the server some actual variables to use.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+
+    let thankYouCards = [];
+
+    for (let i = 0; i < namesArray.length ; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+}
+
+let countDownNumber = 10;
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10);
